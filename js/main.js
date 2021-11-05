7ce30b52ed48f1c67c2bbe528acab56a1a0914a2
mdeias <mdeias@icloud.com>
2021-11-05 16:53:52 +0100

    stampa dei valori non annidati

--- js/main.js
+++ js/main.js
@@ -55,3 +55,65 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro javascript in cui:
+// - Utilizzaimo la base dati fornita e anche il codce HTML/CSS
+// - Gli oggetti nell’array di oggetti rappresentano tutti i post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine (non tutti i post devono avere una immagine), numero di likes.
+// - Stampiamo i post del nostro feed.
+// - Sotto al nome NON inseire il calcolo della data (es 4 mesi fa) ma la data i formato italiano (GG/MM/AAAA)
+
+stampaValori(posts);
+
+// creo una funzione che stampa i valori dell'array
+
+function stampaValori(nomeArray) {
+    // recupero il contenitore con quary selector sulla quale appenderò gli oggetti
+    const contenitore = document.getElementById("container");
+    // resetto il contenitore 
+    contenitore.innerHTML = "";
+    // stampo con un ciclo for tutti i valori degli oggetti
+    for(let valore in nomeArray){
+        // creo il box post 
+        const boxPost = document.createElement("div");
+        // aggiungo la classe post 
+        boxPost.classList.add("post");
+        // scrivo l'html al suo interno
+        boxPost.innerHTML =
+        `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${nomeArray[valore].image}" alt="User image">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${nomeArray[valore].author}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${nomeArray[valore].content}</div>
+        <div class="post__image">
+            <img src="${nomeArray[valore].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${nomeArray[valore].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    
+        `;
+        // appendiamo il tutto al contenitore 
+        contenitore.append(boxPost);
+    }
+}
+
